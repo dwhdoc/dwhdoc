a98e4380dfb46f790569d911de80527ea1576e3c
dwhdoc <112938238+dwhdoc@users.noreply.github.com>
2022-10-24 18:24:38 +0800

    Update q8_youngblood.sql

--- q8_youngblood.sql
+++ q8_youngblood.sql
@@ -2,4 +2,4 @@
 select distinct RegionDescription, FirstName, LastName, BirthDate
 from 'Employee', 'EmployeeTerritory', 'Territory', 'Region'
 where (Employee.Id = EmployeeTerritory.EmployeeId and EmployeeTerritory.TerritoryId = Territory.Id and Territory.RegionId = Region.Id and exists (select * from (select distinct RegionDescription as 'Re', max(BirthDate) as 'Bi' from 'Employee', 'EmployeeTerritory','Territory','Region' where Employee.Id = EmployeeTerritory.EmployeeId and EmployeeTerritory.TerritoryId = Territory.Id and Territory.RegionId = Region.Id group by RegionDescription) as 'EmI' where RegionDescription = EmI.Re and BirthDate = EmI.Bi))
-order by RegionDescription
+order by Region.Id
